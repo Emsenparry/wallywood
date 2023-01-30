df9b3488b21412b9c290e258d9edd63f8896dc57
Emma Nielsen <Emmanielsen843@gmail.com>
2023-01-30 09:23:02 +0100

    Start up project
    Npx create-react-app
    Npm i styled-components and sass
    Create new folders, delete files and other folder that arent important for us.

---
+++ wallywood/src/App.jsx
@@ -0,0 +1,14 @@
+import { ContainerStyle }from "./components/Container.style";
+
+const App = () => {
+  return (
+    <div className="App">
+      <ContainerStyle>
+        fufhu
+      </ContainerStyle>
+    </div>
+      
+  );
+}
+
+export default App;
