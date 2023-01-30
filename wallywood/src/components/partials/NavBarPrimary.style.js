4304bebf00ed4d6112394cafd0468cb1024b35b0
Emma Nielsen <Emmanielsen843@gmail.com>
2023-01-30 14:02:32 +0100

    border color

--- wallywood/src/components/partials/NavBarPrimary.style.js
+++ wallywood/src/components/partials/NavBarPrimary.style.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 import { ClearList } from "../styled/Mixins";
 
 export const NavBarPrimaryStyle = styled.nav`
-    border: solid red 1px;
     ul{
         display: flex;
         ${ClearList}
