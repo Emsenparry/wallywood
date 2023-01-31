669269001d90b0fdd03f5de163399f8b415c2cff
Emma Nielsen <Emmanielsen843@gmail.com>
2023-01-31 14:09:30 +0100

    axios

--- wallywood/src/Pages/Posters/Posters.jsx
+++ wallywood/src/Pages/Posters/Posters.jsx
@@ -1,11 +1,27 @@
+import { useState, useEffect } from "react"
 import { ContentWrapper } from "../../components/app/contentwrapper/ContentWrapper"
 
 const Posters = () => {
     return(
         <ContentWrapper
             title="Plakater" description="Se vores udvalg af plakater">
+                <section><PosterList /></section>
         </ContentWrapper>
     )
+}
+
+const PosterList = () => {
+    const [apiData, setApiData] = useState([])
+
+    useEffect(() => {
+        const url = `http://localhost:4000/poster`
+
+        const getData = async () => {
+            try{
+                const result = await axios.get(url)
+            }
+        }
+    })
 }
 
 export default Posters
