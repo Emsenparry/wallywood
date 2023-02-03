fb11ba26ec51f376fbbb124cdf9332439e730de1
Emma Nielsen <Emmanielsen843@gmail.com>
2023-02-03 11:46:32 +0100

    commits

--- wallywood/src/Pages/Posters/Posters.jsx
+++ wallywood/src/Pages/Posters/Posters.jsx
@@ -1,13 +1,22 @@
 import { ContentWrapper } from "../../components/app/contentwrapper/ContentWrapper"
+import { usePosterData } from "../../components/app/posterlist/PosterGenre";
 
 
 
 const Plakater = () => {
+    const { posterGenre } = usePosterData()
+
     return(
         <div>
              <ContentWrapper
                 title="Plakater" description="Se vores udvalg af plakater">
-                <section></section>
+                <section>
+                {posterGenre && posterGenre.map(poster => {
+                        return(
+                            <h1>lol</h1>
+                        )
+                    })}
+                </section>
         </ContentWrapper>
         </div>
     )
