ab86e8bb0ec53bd166f1704ad9272a90862abe57
Emma Nielsen <Emmanielsen843@gmail.com>
2023-02-09 12:24:30 +0100

    login system

--- wallywood/src/Pages/Login/Login.jsx
+++ wallywood/src/Pages/Login/Login.jsx
@@ -1,8 +1,10 @@
 import { ContentWrapper } from "../../components/app/contentwrapper/ContentWrapper"
 import axios from "axios"
+import { useAuth } from "../../components/providers/AuthProvider"
 
 
 const Login = () => {
+    const { loginData, setLoginData } = useAuth()
     
 
     const submitHandle = async e => {
@@ -13,33 +15,46 @@ const Login = () => {
         const endpoint = `http://localhost:4000/login`
         try{
             const result = await axios.post(endpoint, formdata)
-            handleSessionData(result)
+            handleSessionData(result.data.access_token)
         } catch(err) {
             console.error(`Kunne ikke logge ind: ${err}`)
         }
     }
 
-    const handleSessionData = data => {
+    const handleSessionData = (data) => {
         if(data) {
             sessionStorage.setItem('token', JSON.stringify(data))
+            setLoginData(data)
         }
+    }
+
+    const Logout = () => {
+        sessionStorage.removeItem('token')
+        setLoginData('')
     }
 
     return(
         <ContentWrapper title="Login">
-            <form method="POST">
+            {!loginData ? (
+                <form method="POST">
                 <div>
                     <label htmlFor="username">Brugernavn:</label>
                     <input type="text" name="username" />
                 </div>
                 <div>
-                    <label htmlFor="adgangskode">Adgangskode:</label>
+                    <label htmlFor="Adganskode">Adgangskode:</label>
                     <input type="password" name="password" />
                 </div>
                 <div>
                     <button type="button" onClick={e => submitHandle(e)}>Login</button>
                 </div>
             </form>
+            ) : (
+                <p>
+                    Du er logget ind
+                    <button onClick={() => Logout()}>Log ud</button>
+                </p>
+            )}
         </ContentWrapper>
     )
 }
--- wallywood/src/index.js
+++ wallywood/src/index.js
@@ -6,16 +6,19 @@ import { theme } from './components/styled/Theme'
 import { GlobalStyle } from './components/styled/Global.style'
 import { BrowserRouter } from 'react-router-dom'
 import { PosterProvider } from './components/app/posterlist/PosterList';
+import { AuthProvider } from './components/providers/AuthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <PosterProvider>
     <BrowserRouter>
+    <AuthProvider>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
         <App />
     </ThemeProvider>
+    </AuthProvider>
     </BrowserRouter>
     </PosterProvider>
   </React.StrictMode>
