1e611ab5cc4fc7583ceb29a9dc3d355b5526a8fe
Emma Nielsen <Emmanielsen843@gmail.com>
2023-02-09 08:17:04 +0100

    margins

--- wallywood/src/Pages/omos/OmOs.style.js
+++ wallywood/src/Pages/omos/OmOs.style.js
@@ -4,6 +4,10 @@ export const OmOsStyle = styled.div`
     h2{
         color: ${props => props.theme.colors.secondary};
         font-size: 2em;
-        margin: 1.2em 0 0.5em 0;
+        margin: 1em 0 0 0;
     }
+    figure{
+        margin: 0;
+    }
+    
 `
